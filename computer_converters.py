a4f155fc4d2814a5c50d93d068715ec16e691d6f
Jose Ricardo de Oliveira Damico <jose.damico@scicrop.com>
2021-10-29 19:16:01 -0300

    improved convertions: wip

--- computer_converters.py
+++ computer_converters.py
@@ -1,29 +1,40 @@
 #!/usr/bin/python3
 
-in_binary_str = input("Please enter a binary string:\n")
 
-def convert(binary_str, is_negative):
-    array_len = len(binary_str)
-    binary_array = []
-    for e in binary_str:
-        int_e = int(e)
-        if int_e == 0 or int_e == 1:
-            binary_array.append(int_e)
-        else:
+def switch_base(base):
+    switcher = {
+        "2": "01",
+        "8": "01234567",
+        "10": "0123456789",
+        "16": "0123456789ABCDEF"
+    }
+    return switcher.get(base, "Invalid base")
+
+
+def convert_to_decimal(number_str, is_negative, in_base_str):
+    array_len = len(number_str)
+    number_array = []
+    int_base = int(in_base_str)
+    for e in number_str.upper():
+        base_elements_array = switch_base(in_base_str)
+        if e not in base_elements_array:
             raise Exception()
+        else:
+            number_array.append(base_elements_array.index(e))
+
     if not is_negative:
-        binary_array.reverse()
+        number_array.reverse()
     exp = 0
     result = 0
     signal = 1
     if is_negative:
         signal = -1
         exp = 1
-    for e in binary_array:
+    for e in number_array:
         if exp >=0:
             exp = exp * signal
-        exp_calc = e*(2**exp)
-        print(e,'*',2,'E',exp,'=',exp_calc)
+        exp_calc = e*(int_base**exp)
+        print(e,'*',int_base,'E',exp,'=',exp_calc)
         result = exp_calc + result
         if exp < 0:
             exp = exp - 1
@@ -33,15 +44,39 @@ def convert(binary_str, is_negative):
     return result
 
 
-try:
-    result = 0
-    split = in_binary_str.split('.')
-    if len(split) > 2:
-        raise Exception()
-    else:
-        result = convert(split[0], False)
-        if len(split) == 2:
-            result = result + convert(split[1], True)
-    print(result)
-except Exception as inst:
-    print('The input is not a binary value.')
+def main():
+    try:
+        in_number_str = input("Please enter a number:\n")
+        in_base_str = input("Please enter the number base:\n")
+        result = 0
+        split = in_number_str.split('.')
+        if len(split) > 2:
+            raise Exception()
+        else:
+            result = convert_to_decimal(split[0], False, in_base_str)
+            if len(split) == 2:
+                result = result + convert_to_decimal(split[1], True, in_base_str)
+        print(result)
+    except Exception as inst:
+        print('The input is not a binary value.', type(inst))
+
+
+def prepare_from_decimal():
+    try:
+        in_number_str = input("Please enter a number:\n")
+        in_base_str = input("Please enter the number base:\n")
+        result = 0
+        split = in_number_str.split('.')
+        if len(split) > 2:
+            raise Exception()
+        else:
+            result = convert_to_decimal(split[0], False, in_base_str)
+            if len(split) == 2:
+                result = result + convert_to_decimal(split[1], True, in_base_str)
+        print(result)
+    except Exception as inst:
+        print('The input is not a binary value.', type(inst))
+
+
+main()
+
